35a1376746337d1e578c3ed156f73428763a578d
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-20 13:38:19 +0100

    fully functional code with a fully functional unit test (with added error handling)

--- mainCode.py
+++ mainCode.py
@@ -1,21 +1,35 @@
 import numpy as np
 
 def date_time_calc(date_inputted=None):
-    """Function which performs 'today - user input date' calculation."""
+    """
+    Function which returns today - date_time_calc
+    """
     today = np.datetime64('today')
     if date_inputted is None:
-        date_inputted = np.datetime64(input("Insert a date (YYYY-MM-DD): "))
+        #only asks is no date inputted -> necessary for unit tests
+        while True:
+            try:
+                date_inputted = np.datetime64(input("Insert a date (YYYY-MM-DD): "))
+                
+                if date_inputted > today:
+                    print("Date cannot be a future date.")
+                else:
+                    date_inputted = date_inputted
+                    break
+            except ValueError:
+                print("Must be in format YYYY-MM-DD")
+    else:
+        # if date_inputted is None (unit_test) converts to np.datetime64
+        date_inputted = np.datetime64(date_inputted)
     
-    date_inputted = np.datetime64(date_inputted)
-
     num_days = today - date_inputted
     return num_days
 
-print(date_time_calc())
-
-
-#using xfill{2} using it so that users input seperately.
+#using xfill{2} using it so that users input values seperately maybe
 
 if __name__ == "__main__":
     date_time_result= date_time_calc()  # calls function and asks for input
     print(f"Number of days: {date_time_result}")
+
+
+
--- unitTest.py
+++ unitTest.py
@@ -4,15 +4,12 @@ import numpy as np
 # define the unit tests
 class my_unit_tests(unittest.TestCase):
 
-        # tests if date difference is correctly caculated
     def test_date_difference(self):
-        """"""
-        today = np.datetime64('2025-10-20')
-        user_inputed_test_date = date_time_calc("2025-10-10")
-        result = today - np.datetime64(user_inputed_test_date)
-        expected_result = np.timedelta64(10, 'D')  #expected result
+        """Test that date difference is calculated correctly."""
+        actual_result = date_time_calc("2025-10-10")
+        expected_result = np.datetime64('today') - np.datetime64("2025-10-10")
+        self.assertEqual(actual_result, expected_result)
 
-        self.assertEqual(expected_result, result)
 
 
     # run the tests
